e9f5580604b482044ea16fdc9d58a880020dc795
prasun <prasunanand.bitsp@gmail.com>
2016-06-11 20:36:48 +0530

    performance graphs for matrix addition, multiplication, division

--- script.rb
+++ script.rb
@@ -15,89 +15,49 @@ class ResultCollect
     result = {}
 
     store = {}
-    store[:addition] = []
-    store[:subtraction] = {}
-    store[:multiplication] = {} 
+    store[:elementsCount] = []
+    store[:features] = {ruby: {}, jruby: {}}
+    store[:features][:ruby][:addition] = []
+    store[:features][:ruby][:subtraction] = []
+    store[:features][:ruby][:multiplication] = []
+    store[:features][:ruby][:determinant] = []
 
+    store[:features][:jruby][:addition] = []
+    store[:features][:jruby][:subtraction] = []
+    store[:features][:jruby][:multiplication] = []
+    store[:features][:jruby][:determinant] = []
 
-    shape1 = [10, 1]
-    shape2 = [1, 10]
-    shape3 = [1, 10]
+    shapeArray = [
+                  [10,10],[50,50],[100,100],[500,500],
+                  [1000,1000],[2000,2000],[3000,3000],
+                  [4000,4000],
+                  # [5000,5000],[6000,6000],
+                  # [7000,7000], [8000,8000],[9000,9000],
+                  # [10000,10000]
+                ]
 
+    shapeArray.each do |shape|
+      store[:elementsCount] << shape[0]*shape[1]
 
-    elements1 = Array.new(10) { rand(1...999999) }
-    elements2 = Array.new(10) { rand(1...999999) }
-    elements3 = Array.new(10) { rand(1...999999) }
+      elements1 = Array.new(shape[0]*shape[1]) { rand(1...999999) }
+      elements2 = Array.new(shape[0]*shape[1]) { rand(1...999999) }
+      nmatrix1 = NMatrix.new(shape, elements1, dtype: :float32)
+      nmatrix2 = NMatrix.new(shape, elements2, dtype: :float32)
 
-    a = NMatrix.new(shape1, elements1 , dtype: :float32)
-    b = NMatrix.new(shape2, elements2 , dtype: :float32)
-    c = NMatrix.new(shape3, elements3 , dtype: :float32)
-
-    store[:addition] << Benchmark.measure{b + c}.to_s.tr('()', '').split(" ")[3].to_i
-    store[:subtraction][:e10] = Benchmark.measure{b - c}.to_s.tr('()', '').split(" ")
-    store[:multiplication][:e10] = Benchmark.measure{a.dot(b)}.to_s.tr('()', '').split(" ")
-
-
-
-    shape1 = [100, 10]
-    shape2 = [10, 100]
-    shape3 = [10, 100]
-
-
-    elements1 = Array.new(1000) { rand(1...999999) }
-    elements2 = Array.new(1000) { rand(1...999999) }
-    elements3 = Array.new(1000) { rand(1...999999) }
-
-    a = NMatrix.new(shape1, elements1 , dtype: :float32)
-    b = NMatrix.new(shape2, elements2 , dtype: :float32)
-    c = NMatrix.new(shape3, elements3 , dtype: :float32)
-
-    store[:addition] << Benchmark.measure{b + c}.to_s.tr('()', '').split(" ")[3].to_i
-    store[:subtraction][:e1000] = Benchmark.measure{b - c}.to_s.tr('()', '').split(" ")
-    store[:multiplication][:e1000] = Benchmark.measure{a.dot(b)}.to_s.tr('()', '').split(" ")
-
-
-    shape1 = [1000, 100]
-    shape2 = [100, 1000]
-    shape3 = [100, 1000]
-
-
-    elements1 = Array.new(100000) { rand(1...999999) }
-    elements2 = Array.new(100000) { rand(1...999999) }
-    elements3 = Array.new(100000) { rand(1...999999) }
-
-    a = NMatrix.new(shape1, elements1 , dtype: :float32)
-    b = NMatrix.new(shape2, elements2 , dtype: :float32)
-    c = NMatrix.new(shape3, elements3 , dtype: :float32)
-
-    store[:addition] << Benchmark.measure{b + c}.to_s.tr('()', '').split(" ")[3].to_i
-    store[:subtraction][:e100000] = Benchmark.measure{b - c}.to_s.tr('()', '').split(" ")
-    store[:multiplication][:e100000] = Benchmark.measure{a.dot(b)}.to_s.tr('()', '').split(" ")
-
-
-    shape1 = [10000, 1000]
-    shape2 = [1000, 10000]
-    shape3 = [1000, 10000]
-
-
-    elements1 = Array.new(10000000) { rand(1...999999) }
-    elements2 = Array.new(10000000) { rand(1...999999) }
-    elements3 = Array.new(10000000) { rand(1...999999) }
-
-    a = NMatrix.new(shape1, elements1 , dtype: :float32)
-    b = NMatrix.new(shape2, elements2 , dtype: :float32)
-    c = NMatrix.new(shape3, elements3 , dtype: :float32)
-
-    store[:addition] << Benchmark.measure{b + c}.to_s.tr('()', '').split(" ")[3].to_i
-    store[:subtraction][:e10000000] = Benchmark.measure{b - c}.to_s.tr('()', '').split(" ")
-
-    if jruby?
-      result[:jruby] = store
-    else
-      result[:ruby] = store
+      if jruby?
+        store[:features][:jruby][:addition] << [ shape[0]*shape[1], Benchmark.measure{nmatrix1 + nmatrix2}.to_s.tr('()', '').split(" ")[3].to_f ]
+        store[:features][:jruby][:subtraction] << [ shape[0]*shape[1], Benchmark.measure{nmatrix1 - nmatrix2}.to_s.tr('()', '').split(" ")[3].to_f ]
+        store[:features][:jruby][:multiplication] << [ shape[0]*shape[1], Benchmark.measure{nmatrix1.dot(nmatrix2)}.to_s.tr('()', '').split(" ")[3].to_f ]
+        store[:features][:jruby][:determinant] << [ shape[0]*shape[1], Benchmark.measure{nmatrix1.det_exact}.to_s.tr('()', '').split(" ")[3].to_f ]
+      else
+        store[:features][:ruby][:addition] << [ shape[0]*shape[1], Benchmark.measure{nmatrix1 + nmatrix2}.to_s.tr('()', '').split(" ")[3].to_f ]
+        store[:features][:ruby][:subtraction] << [ shape[0]*shape[1], Benchmark.measure{nmatrix1 - nmatrix2}.to_s.tr('()', '').split(" ")[3].to_f ]
+        store[:features][:ruby][:multiplication] << [ shape[0]*shape[1], Benchmark.measure{nmatrix1.dot(nmatrix2)}.to_s.tr('()', '').split(" ")[3].to_f ]
+        store[:features][:ruby][:determinant] << [ shape[0]*shape[1], Benchmark.measure{nmatrix1.det_exact}.to_s.tr('()', '').split(" ")[3].to_f ]
+      end
     end
-
-    puts result.to_json
+    puts "======================================================="
+    puts store.to_json
   end
 end
 # (0...100).each do |i|
